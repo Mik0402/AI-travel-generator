d6365bf916c3a59eaa087a0edd5649418864b551
Mik0402 <mikaelalammers@yahoo.com>
2023-12-30 16:44:55 -0600

    Adding Java for the project

--- src/java.js
+++ src/java.js
@@ -1,1 +1,13 @@
+function generateItinerary(event) {
+  event.preventDefault();
 
+  new Typewriter("#itinerary", {
+    strings: "Places in Paris",
+    autoStart: true,
+    cursor: " ",
+    delay: 1,
+  });
+}
+
+let itineraryFormElement = document.querySelector("#itinerary-generator-form");
+itineraryFormElement.addEventListener("submit", generateItinerary);
